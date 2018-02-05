0370c91e5546c4b816fa7a688c8c1b9b1d5eca9e
laitinent <toni.laitinen@hamk.fi>
2018-02-05 13:31:43 +0200

    Atomi ja Viesti

---
+++ src/harjoitus1/Atomi.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package harjoitus1;
+
+/**
+ *
+ * @author tlaitinen
+ */
+public class Atomi {
+    private int protonit;
+    private int elektronit;
+
+    public int getProtonit() {
+        return protonit;
+    }
+
+    public void setProtonit(int protonit) {
+        this.protonit = protonit;
+    }
+
+    public int getElektronit() {
+        return elektronit;
+    }
+
+    public void setElektronit(int elektronit) {
+        this.elektronit = elektronit;
+    }
+}
---
+++ src/harjoitus1/Viesti.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package harjoitus1;
+
+/**
+ *
+ * @author tlaitinen
+ */
+public class Viesti {
+    private String sisältö;
+
+    public String getSisältö() {
+        return sisältö;
+    }
+
+    public void setSisältö(String sisältö) {
+        this.sisältö = sisältö;
+    }
+    
+}
